77c6273f08d1441e713d76cbc9f951337b85ac82
Kamil Balogun <kamilbalogun1@gmail.com>
2020-03-13 15:14:37 +0100

    Add time

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let time = new Date().toLocaleString();
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>
+            Welcome to React
+          </h2>
+          {/* <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a> */}
+        </header>
+        <div>
+          <p className="App-intro">
+          Hi {this.props.name}
+          </p>
+          <p className="App-clock">
+            the Time is { time }
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
